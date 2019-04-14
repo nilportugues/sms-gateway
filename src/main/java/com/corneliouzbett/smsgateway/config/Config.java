8eb52a1e888802b8391d63a928fe3cfb8c29f108
corneliouzbett <kbett68@gmail.com>
2019-04-14 14:32:51 +0300

    added configuration file

---
+++ src/main/java/com/corneliouzbett/smsgateway/config/Config.java
@@ -0,0 +1,7 @@
+package com.corneliouzbett.smsgateway.config;
+
+public class Config {
+    public static final String ACCOUNT_SID = "AC7ca67ed5494ac7e30fe4d8b51e63edb8";
+    public static final String AUTH_TOKEN = "84445cdea54a1bc01796f03f9e44a1c2";
+    public static final String OWNER_CONTACT = "+13212509427";
+}
