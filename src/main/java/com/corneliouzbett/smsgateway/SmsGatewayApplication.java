a68796e8d117f0a9355a70c66fb11164dec6cee1
corneliouzbett <kbett68@gmail.com>
2019-04-14 14:25:57 +0300

    initial commit

---
+++ src/main/java/com/corneliouzbett/smsgateway/SmsGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.corneliouzbett.smsgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsGatewayApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SmsGatewayApplication.class, args);
+	}
+
+}
