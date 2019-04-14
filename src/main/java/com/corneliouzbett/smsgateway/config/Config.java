cd7bfe98fb206ec51df6747a87bd7250214b0daa
corneliouz bett <kbett68@gmail.com>
2019-04-14 17:27:33 +0300

    Update Config.java

--- src/main/java/com/corneliouzbett/smsgateway/config/Config.java
+++ src/main/java/com/corneliouzbett/smsgateway/config/Config.java
@@ -1,7 +1,7 @@
 package com.corneliouzbett.smsgateway.config;
 
 public class Config {
-    public static final String ACCOUNT_SID = "AC7ca67ed5494ac7e30fe4d8b51e63edb8";
-    public static final String AUTH_TOKEN = "84445cdea54a1bc01796f03f9e44a1c2";
-    public static final String OWNER_CONTACT = "+13212509427";
+    public static final String ACCOUNT_SID = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String AUTH_TOKEN = "XXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String OWNER_CONTACT = "+CCCCCCCC";
 }
